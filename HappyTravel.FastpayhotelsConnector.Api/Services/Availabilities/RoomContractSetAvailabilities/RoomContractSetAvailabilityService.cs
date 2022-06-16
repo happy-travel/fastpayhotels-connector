dc5de1798cd26a6024bf5264f0b06117b1f86a90
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-06-16 09:55:58 +0400

    Base configuration for Api project

---
+++ HappyTravel.FastpayhotelsConnector.Api/Services/Availabilities/RoomContractSetAvailabilities/RoomContractSetAvailabilityService.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using HappyTravel.BaseConnector.Api.Services.Availabilities.RoomContractSetAvailabilities;
+using HappyTravel.EdoContracts.Accommodations;
+
+namespace HappyTravel.FastpayhotelsConnector.Api.Services.Availabilities.RoomContractSetAvailabilities;
+
+public class RoomContractSetAvailabilityService : IRoomContractSetAvailabilityService
+{
+    public Task<Result<RoomContractSetAvailability?>> Get(string availabilityId, Guid roomContractSetId, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
