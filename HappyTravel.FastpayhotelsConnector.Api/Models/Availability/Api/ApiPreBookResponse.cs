e0d8e470b95856171d11737263fe7d0fab374ca5
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-07-13 16:34:20 +0400

    Added MessageId

--- HappyTravel.FastpayhotelsConnector.Api/Models/Availability/Api/ApiPreBookResponse.cs
+++ HappyTravel.FastpayhotelsConnector.Api/Models/Availability/Api/ApiPreBookResponse.cs
@@ -2,7 +2,7 @@
 
 namespace HappyTravel.FastpayhotelsConnector.Api.Models.Availability.Api;
 
-public class ApiPreBookResponse
+public class ApiPreBookResponse : ApiMessage
 {
     [JsonPropertyName("result")]
     public ApiPreBookRsResult Result { get; set; }
