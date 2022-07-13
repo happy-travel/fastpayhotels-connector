ca88e497bcf2f0063c26df717091a07484b5600b
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-07-13 15:53:29 +0400

    Added base api message model

---
+++ HappyTravel.FastpayhotelsConnector.Api/Models/ApiMessage.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HappyTravel.FastpayhotelsConnector.Api.Models;
+
+public class ApiMessage
+{
+    [JsonPropertyName("messageID")]
+    public string MessageId { get; set; }
+}
