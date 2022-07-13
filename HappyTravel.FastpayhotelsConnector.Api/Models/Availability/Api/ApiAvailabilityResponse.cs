a7f2accc66243adacb04fa4057da0b3a4d8fd87f
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-07-13 15:53:29 +0400

    Added base api message model

--- HappyTravel.FastpayhotelsConnector.Api/Models/Availability/Api/ApiAvailabilityResponse.cs
+++ HappyTravel.FastpayhotelsConnector.Api/Models/Availability/Api/ApiAvailabilityResponse.cs
@@ -2,7 +2,7 @@
 
 namespace HappyTravel.FastpayhotelsConnector.Api.Models.Availability.Api;
 
-public class ApiAvailabilityResponse
+public class ApiAvailabilityResponse : ApiMessage
 {
     [JsonPropertyName("hotelAvails")]
     public List<ApiHotelAvail> HotelAvails { get; set; }
