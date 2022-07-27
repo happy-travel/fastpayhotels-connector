d54bc8047e465c062b2715ce43671972df898b79
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-07-27 10:31:56 +0400

    Changed the method of obtaining an authorization token

---
+++ HappyTravel.FastpayhotelsConnector.Common/Models/TokenErrorMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HappyTravel.FastpayhotelsConnector.Common.Models;
+
+public class TokenErrorMessage
+{
+    [JsonPropertyName("error")]
+    public string Error { get; set; }
+
+    [JsonPropertyName("error_description")]
+    public string ErrorDescription { get; set; }
+}
