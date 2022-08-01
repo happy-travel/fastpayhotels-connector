a549e20424b6757894677806472284fe5642c468
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-08-01 18:08:53 +0400

    Fixed by comments

--- HappyTravel.FastpayhotelsConnector.Common/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ HappyTravel.FastpayhotelsConnector.Common/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -8,13 +8,22 @@ namespace HappyTravel.FastpayhotelsConnector.Common.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// The method adds the http client and services needed to get the token
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection</param>
+        /// <param name="tokenEndPoint">tokenEndPoint</param>
+        /// <returns></returns>
         public static IServiceCollection AddTokenAuthHeaderService(this IServiceCollection services, string tokenEndPoint)
-        {             
+        {
+            var httpMessageHandlerLifeTimeMinutes = TimeSpan.FromMinutes(5);
+            const int RetryCount = 3;
+
             services.AddHttpClient(Constants.FastpayhotelsTokenClient, client =>
                 {
                     client.BaseAddress = new Uri(tokenEndPoint);
                 })
-                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .SetHandlerLifetime(httpMessageHandlerLifeTimeMinutes)
                 .AddPolicyHandler(GetDefaultRetryPolicy());
 
             return services.AddTransient<TokenAuthHeaderClient>()
@@ -29,7 +38,7 @@ namespace HappyTravel.FastpayhotelsConnector.Common.Infrastructure.Extensions
                 return HttpPolicyExtensions
                     .HandleTransientHttpError()
                     .OrResult(msg => msg.StatusCode == HttpStatusCode.ServiceUnavailable)
-                    .WaitAndRetryAsync(3, attempt
+                    .WaitAndRetryAsync(RetryCount, attempt
                         => TimeSpan.FromSeconds(Math.Pow(1.5, attempt)) + TimeSpan.FromMilliseconds(jitter.Next(0, 100)));
             }
         }
--- HappyTravel.FastpayhotelsConnector.Common/Models/TokenErrorMessage.cs
+++ HappyTravel.FastpayhotelsConnector.Common/Models/TokenErrorMessage.cs
@@ -2,11 +2,20 @@
 
 namespace HappyTravel.FastpayhotelsConnector.Common.Models;
 
+/// <summary>
+/// Describes the error that occurred while getting the token
+/// </summary>
 public class TokenErrorMessage
 {
+    /// <summary>
+    /// Error
+    /// </summary>
     [JsonPropertyName("error")]
     public string Error { get; set; }
 
+    /// <summary>
+    /// Error description
+    /// </summary>
     [JsonPropertyName("error_description")]
     public string ErrorDescription { get; set; }
 }
